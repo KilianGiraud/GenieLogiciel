7f17741279fc2f38afacf434ec96c8672da3d0c3
kigiraud <kigiraud@a206pc11L.local.isima.fr>
2025-09-19 15:26:01 +0200

    Ajouts/Modifications du fichier Tests.md pour ajouter les derniers tests et conception des tests unitaires pour les classes player et Affichage dans le fichier UnitTests.java. TODO : Concevoir les tests unitaires pour la classe UpdatePlayer

--- Tests.md
+++ Tests.md
@@ -10,53 +10,53 @@
 
 ### UpdatePlayer / majFinDeTour
 
+- "maj" : Executer la fonction
+- "Mort" : Vérifier si cela fonctionne quand le joueur est mort (0 PV)
+- "Le cimetière de if" : Vérifier toutes les clauses "if / else / elif" (y'en a trop pour tout énumérer)
+
 ### UpdatePlayer / addXP *assez de tests*
 
-- "Player level" : Test ajout négatif de niveau
+- "Player level" : Test ajout négatif de niveau 
 
 - "Player level 2" : Ajouter beaucoup d'XP pour passer de niveau et vérifier ensuite le **put**
 
 ### player / removeMoney *assez de tests*
 
-- "Remove money" : Test de retirer plus d'argent que le joueur n'en possède
+- "Remove money" : Test de retirer plus d'argent que le joueur n'en possède *fonction testNegativeMoney*
 
-- "Remove money 2" : Retirer moins d'argent que ce que le joueur possède
+- "Remove money 2" : Retirer moins d'argent que ce que le joueur possède *fonction testPositiveMoney*
 
 
 ### player / addMoney *assez de tests*
 
-- "Player money" : Test d'ajout d'argent de valeur négative
+- "Player money" : Test d'ajout d'argent de valeur négative *fonction testAddNegativeMoney*
 
-- "Player money 2" : Test d'ajout de format String dans la variable "argent"
+- "Player money 2" : Test d'ajout d'argent = 0 *fonction testAddNoMoney*
 
-- "Player money 3" : Test d'ajout d'argent = 0
-
+- "Player money 3" : Test d'ajout normal
 ### player / retrieveLevel *assez de tests*
 
-- "Player level 3" : Tester d'arriver au level 2
+- "Player level 3" : Tester d'arriver au level 2 *fonction testGoToLvl2*
 - "Player level 4" : Tester d'arriver au level 3
-- "Player level 5" : Tester d'arriver au level 4
+- "Player level 5" : Tester d'arriver au level 4 et 5 *fonction testGoToLvl4and5*
 
 
 ### player / getXP *assez de tests*
 
-- "Player XP" : Test de la fonction getXP (affichage aussi)
+- "Player XP" : Test de la fonction getXP (affichage aussi) *fonction testXP*
 
 ### player / player *assez de tests*
 
-- "Player name" : Test basique avec nom du joueur correctement mit
+- "Player name" : Test basique avec nom du joueur correctement mit *fonction testPlayerName*
 
-- "Player class" : Test d'une classe non prise en charge par le code source (ex : BARBARIAN, SORCERER)
+- "Player class" : Test de l'affichage des classes et de si la classe est **null** *fonction testClass*
 
-- "Player class 2" : Test des différentes classes prises en charge (AVENTURER, DWARF, ARCHER)
-
-- "Player class 3" : Test du mauvais type de variable
-
-- "Player class 4" : Test de classe **null**
 
 ### Affichage / afficherJoueur *assez de tests*
 
-- "Inventaire" : Remplir l'inventaire pour tester l'affichage
+- "Inventaire" : Remplir l'inventaire pour tester l'affichage *fonction testAddItem*
+
+- "Affichage" : Test de la fonction générale "Affichage" *fonction testAffichage*
 
 -----------------------------
 
--- src/test/java/re/forestier/edu/UnitTests.java
+++ src/test/java/re/forestier/edu/UnitTests.java
@@ -1,15 +1,19 @@
 package re.forestier.edu;
 
 import org.junit.jupiter.api.*;
+import re.forestier.edu.rpg.UpdatePlayer;
+import re.forestier.edu.rpg.Affichage;
 import re.forestier.edu.rpg.player;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
 public class UnitTests {
 
+
+    // Test basique de vérification du nom du joueur
     @Test
     @DisplayName("Sample test")
     void testPlayerName() {
@@ -17,6 +21,9 @@ public class UnitTests {
         assertThat(player.playerName, is("Florian"));
     }
 
+    /* Tests de player / removeMoney */
+
+    //Tentative de retrait d'un montant supérieur à la quantité d'argent du joueur
     @Test
     @DisplayName("Impossible to have negative money")
     void testNegativeMoney() {
@@ -24,10 +31,147 @@ public class UnitTests {
 
         try {
             p.removeMoney(200);
-        } catch (IllegalArgumentException e) {
-            return;
+        } catch (Exception e) {
+            assertThat(e, is(instanceOf(IllegalArgumentException.class)));
         }
-        fail();
+    }
+
+
+    // Tentative classique de retrait d'argent
+    @Test
+    @DisplayName("Money removed")
+    void testPositiveMoney() {
+        player p = new player("Kilian", "Denver le dernier Dinosaure", "ADVENTURER", 100, new ArrayList<>());
+
+        try {
+            p.removeMoney(50);
+        } catch (Exception e) {
+            assertThat(e, is(instanceOf(IllegalArgumentException.class)));
+        }
+    }
+
+    /* Tests de player / addMoney */
+
+
+    // Tentative de d'ajouter une valeur négative d'argent
+    @Test
+    @DisplayName("Impossible to add negative money")
+    void testAddNegativeMoney() {
+        player p = new player("Kilian", "Denver le dernier Dinosaure", "ADVENTURER", 100, new ArrayList<>());
+
+        try {
+            p.addMoney(-40);
+        } catch (Exception e) {
+            assertThat(e, is(instanceOf(IllegalArgumentException.class)));
+        }
+    }
+
+
+    // Tentative d'ajout d'argent à 0
+    @Test
+    @DisplayName("Nothing happened")
+    void testAddNoMoney() {
+        player p = new player("Kilian", "Denver le dernier Dinosaure", "ADVENTURER", 100, new ArrayList<>());
+
+        try {
+            p.addMoney(0);
+        } catch (Exception e) {
+            assertThat(e, is(instanceOf(IllegalArgumentException.class)));
+        }
+    }
+
+
+    // Tentative classique d'ajout d'argent
+    @Test
+    @DisplayName("Money added")
+    void testAddMoney() {
+        player p = new player("Kilian", "Denver le dernier Dinosaure", "ADVENTURER", 100, new ArrayList<>());
+
+        try {
+            p.addMoney(100);
+        } catch (Exception e) {
+            assertThat(e, is(instanceOf(IllegalArgumentException.class)));
+        }
+    }
+
+    /* Tests player / retrieveLevel */
+
+
+    // Ajout d'XP afin d'atteindre le Level 2
+    @Test
+    @DisplayName("Access to level 2")
+    void testGoToLvl2() {
+        player p = new player("Kilian", "Denver le dernier Dinosaure", "ADVENTURER", 100, new ArrayList<>());
+
+        try {
+            UpdatePlayer.addXp(p, 58);
+            p.retrieveLevel();
+        } catch (Exception e) {
+            assertThat(e, is(instanceOf(IllegalArgumentException.class)));
+        }
+
+    }
+
+    // Ajout d'XP afin d'atteindre le Level 4 et 5
+    @Test
+    @DisplayName("Access to level 4 and 5")
+    void testGoToLvl4and5() {
+        player p2 = new player("Kilian", "Denver le dernier Dinosaure", "ADVENTURER", 100, new ArrayList<>());
+
+        try {
+            UpdatePlayer.addXp(p2, 120);
+            p2.retrieveLevel();
+        } catch (Exception e) {
+            assertThat(e, is(instanceOf(IllegalArgumentException.class)));
+        }
+    }
+
+    /* Test XP */
+
+    // Initiation d'une variable qui réprésente l'XP du joueur puis vérifier sa valeur
+    @Test
+    @DisplayName("Show User's XP")
+    void testXP() {
+        player p = new player("Kilian", "Denver le dernier Dinosaure", "ADVENTURER", 100, new ArrayList<>());
+        int xp = p.getXp();
+        assertThat(xp, is(0));
+    }
+
+    /* Test player player */
+
+
+    // Test de toutes les classes et du cas où la classe est nulle
+    @Test
+    @DisplayName("Show Player's class")
+    void testClass() {
+        player p = new player("Kilian", "Denver le dernier Dinosaure", "", 100, new ArrayList<>());
+        player p1 = new player("Kilian", "Denver le dernier Dinosaure", "ADVENTURER", 100, new ArrayList<>());
+        player p2 = new player("Kilian", "Denver le dernier Dinosaure", "DWARF", 100, new ArrayList<>());
+        player p3 = new player("Kilian", "Denver le dernier Dinosaure", "ARCHER", 100, new ArrayList<>());
+        assertThat(p.getAvatarClass(), is(nullValue()));
+        assertThat(p1.getAvatarClass(), is("ADVENTURER"));
+        assertThat(p2.getAvatarClass(), is("DWARF"));
+        assertThat(p3.getAvatarClass(), is("ARCHER"));
+    }
+
+    /* Test d'affichage (inventaire) */
+
+
+    // Ajout d'un Item, affichage du joueur puis vérification de l'item dans l'inventaire
+    @Test
+    @DisplayName("Item added")
+    void testAddItem() {
+        player p = new player("Kilian", "Denver le dernier Dinosaure", "ADVENTURER", 100, new ArrayList<>());
+        p.inventory.add("Maximilien");
+        Affichage.afficherJoueur(p);
+        assertThat(p.inventory, hasItem("Maximilien"));
+    }
+
+    // Fonction qui va executer la fonction "Affichage"
+    @Test
+    @DisplayName("Test Affichage")
+    void testAffichage() {
+        new Affichage();
     }
 
 }
