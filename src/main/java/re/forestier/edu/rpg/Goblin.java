75adfe61fdd8563eeea4be2c454c2cbf23292f82
Kilian <kilian.giraud63@gmail.com>
2025-12-05 14:55:36 +0100

    feat: Adding Goblin subclasses | TODO: add healLogic to Goblin

---
+++ src/main/java/re/forestier/edu/rpg/Goblin.java
@@ -0,0 +1,48 @@
+package re.forestier.edu.rpg;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Goblin extends Player{
+
+    public Goblin(String playerName, String avatarName, int money, ArrayList<String> inventory) {
+        super(playerName, avatarName, "GOBLIN", money, inventory);
+    }
+
+    @Override
+    public HashMap<String, Integer> getAbilitiesForLevel(int level) {
+        HashMap<String, Integer> stats = new HashMap<>();
+
+        switch(level) {
+            case 1:
+                stats.put("INT", 2);
+                stats.put("ATK", 2);
+                stats.put("ALC", 1);
+                break;
+            case 2:
+                stats.put("ATK", 3);
+                stats.put("ALC", 4);
+                break;
+            case 3:
+                stats.put("VIS", 1);
+                break;
+            case 4:
+                stats.put("DEF", 1);
+                break;
+            case 5:
+                stats.put("DEF", 2);
+                stats.put("ATK", 4);
+                break;
+        }
+
+        return stats;
+    }
+
+    // Overrided method specific to each player
+
+    @Override
+    protected void healLogic() {
+        // TO IMPLEMENT
+    }
+
+}
